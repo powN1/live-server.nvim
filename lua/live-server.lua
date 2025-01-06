8c8895bdce323983072a6a8fd98679ed551332d5
powN <zeta.lacerta@gmail.com>
2025-01-06 03:11:24 +0100

    Add url notification

--- lua/live-server.lua
+++ lua/live-server.lua
@@ -1,121 +1,122 @@
 local M = {}
 
 local function log(message, level)
-    vim.notify(
-        string.format('live-server.nvim: %s', message),
-        vim.log.levels[level]
-    )
+  vim.notify(
+    string.format('live-server.nvim: %s', message),
+    vim.log.levels[level]
+  )
 end
 
 local job_cache = {}
 
 local function find_cached_dir(dir)
-    local cur = dir
+  local cur = dir
 
-    while not job_cache[cur] do
-        if cur == '/' or string.match(cur, '^[A-Z]:\\$') then
-            return
-        end
-
-        cur = vim.fn.fnamemodify(cur, ':h')
+  while not job_cache[cur] do
+    if cur == '/' or string.match(cur, '^[A-Z]:\\$') then
+      return
     end
 
-    return cur
+    cur = vim.fn.fnamemodify(cur, ':h')
+  end
+
+  return cur
 end
 
 local function is_running(dir)
-    local cached_dir = find_cached_dir(dir)
-    return cached_dir and job_cache[cached_dir]
+  local cached_dir = find_cached_dir(dir)
+  return cached_dir and job_cache[cached_dir]
 end
 
 M.config = {
-    -- 8080 default is commonly used
-    args = { '--port=5555' },
+  -- 8080 default is commonly used
+  args = { '--port=5555' },
 }
 
 M.toggle = function(dir)
-    local running = is_running(dir)
-    if not running then
-        M.start(dir)
-        return
-    end
-    M.stop(dir)
+  local running = is_running(dir)
+  if not running then
+    M.start(dir)
+    return
+  end
+  M.stop(dir)
 end
 
 M.setup = function(user_config)
-    M.config = vim.tbl_deep_extend('force', M.config, user_config or {})
+  M.config = vim.tbl_deep_extend('force', M.config, user_config or {})
 
-    if not vim.fn.executable 'live-server' then
-        log(
-            'live-server is not executable. Ensure the npm module is properly installed',
-            vim.log.levels.ERROR
-        )
-        return
-    end
+  if not vim.fn.executable 'live-server' then
+    log(
+      'live-server is not executable. Ensure the npm module is properly installed',
+      vim.log.levels.ERROR
+    )
+    return
+  end
 
-    local function find_dir(args)
-        local dir = args ~= '' and args or '%:p:h'
-        return vim.fn.expand(vim.fn.fnamemodify(vim.fn.expand(dir), ':p'))
-    end
+  local function find_dir(args)
+    local dir = args ~= '' and args or '%:p:h'
+    return vim.fn.expand(vim.fn.fnamemodify(vim.fn.expand(dir), ':p'))
+  end
 
-    vim.api.nvim_create_user_command('LiveServerStart', function(opts)
-        M.start(find_dir(opts.args))
-    end, { nargs = '?' })
-    vim.api.nvim_create_user_command('LiveServerStop', function(opts)
-        M.stop(find_dir(opts.args))
-    end, { nargs = '?' })
-    vim.api.nvim_create_user_command('LiveServerToggle', function(opts)
-        M.toggle(find_dir(opts.args))
-    end, { nargs = '?' })
+  vim.api.nvim_create_user_command('LiveServerStart', function(opts)
+    M.start(find_dir(opts.args))
+  end, { nargs = '?' })
+  vim.api.nvim_create_user_command('LiveServerStop', function(opts)
+    M.stop(find_dir(opts.args))
+  end, { nargs = '?' })
+  vim.api.nvim_create_user_command('LiveServerToggle', function(opts)
+    M.toggle(find_dir(opts.args))
+  end, { nargs = '?' })
 end
 
 M.start = function(dir)
-    local running = is_running(dir)
+  local running = is_running(dir)
 
-    if running then
-        log('live-server already running', 'INFO')
+  if running then
+    log('live-server already running', 'INFO')
+    return
+  end
+
+  local cmd = { 'live-server', dir }
+  vim.list_extend(cmd, M.config.args)
+
+  local job_id = vim.fn.jobstart(cmd, {
+    on_stderr = function(_, data)
+      if not data or data[1] == '' then
         return
-    end
+      end
 
-    local cmd = { 'live-server', dir }
-    vim.list_extend(cmd, M.config.args)
+      -- Remove color from error
+      log(data[1]:match '.-m(.-)\27', 'ERROR')
+    end,
+    on_exit = function(_, exit_code)
+      job_cache[dir] = nil
 
-    local job_id = vim.fn.jobstart(cmd, {
-        on_stderr = function(_, data)
-            if not data or data[1] == '' then
-                return
-            end
+      -- instance killed with SIGTERM
+      if exit_code == 143 then
+        return
+      end
 
-            -- Remove color from error
-            log(data[1]:match '.-m(.-)\27', 'ERROR')
-        end,
-        on_exit = function(_, exit_code)
-            job_cache[dir] = nil
+      log(string.format('stopped with code %s', exit_code), 'INFO')
+    end,
+  })
 
-            -- instance killed with SIGTERM
-            if exit_code == 143 then
-                return
-            end
-
-            log(string.format('stopped with code %s', exit_code), 'INFO')
-        end,
-    })
-
-    log('live-server started', 'INFO')
-    job_cache[dir] = job_id
+  local port = M.config.args[1]:match("%d+$")
+  log('live-server started on 127.0.0.1:' .. port, 'INFO')
+  job_cache[dir] = job_id
 end
 
 M.stop = function(dir)
-    local running = is_running(dir)
+  local running = is_running(dir)
 
-    if running then
-        local cached_dir = find_cached_dir(dir)
-        if cached_dir then
-            vim.fn.jobstop(job_cache[cached_dir])
-            job_cache[cached_dir] = nil
-            log('live-server stopped', 'INFO')
-        end
+  if running then
+    local cached_dir = find_cached_dir(dir)
+    if cached_dir then
+      vim.fn.jobstop(job_cache[cached_dir])
+      job_cache[cached_dir] = nil
+      log('live-server stopped', 'INFO')
     end
+  end
 end
 
 return M
